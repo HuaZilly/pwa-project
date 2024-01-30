790f7b5c4189608b8c736eae89651d5f8d96a831
Thanh Zill <thanh.hua@balanceinternet.com.au>
2024-01-30 18:38:35 +0700

    PWA0000-1: PWA greeting

---
+++ src/components/GreetingPage/greetingPage.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const hi = {
+    textAlign: "center",
+    margin: "1rem"
+}
+
+
+const wave = {
+    ...hi,
+    fontSize: "5rem",
+}
+
+const GreetingPage = () => {
+    const { who = "nobody" } = useParams();
+    return (
+
+        <div>
+            <h1 style={hi} >Hello, {who}!</h1>
+            <h1 style={wave}>{"\uD83D\uDC4BD"}</h1>
+        </div>
+
+    )
+}
+
+export default GreetingPage;
